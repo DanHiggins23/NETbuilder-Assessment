4b388f75164600069e24f86585e93a8c8d1081b9
DanHiggins23 <danhiggins23@outlook.com>
2018-11-01 21:30:14 +0000

    upload first task completed

---
+++ string_converter/src/converter.java
@@ -0,0 +1,25 @@
+public class converter {
+    public static void main(String[] args) {
+        // Enter text in params below
+        convert("This NETbuilder assessment is way too easy.");
+    }
+
+    public static void convert(String userInput) {
+        String output = "";
+
+        // Convert input into lower case so that correct ascii code is shown
+        userInput = userInput.toLowerCase();
+
+        for (int count = 0; count <= (userInput.length() - 1); count++) {
+            int charCode = Character.codePointAt(userInput, count);
+
+            // Ensures that only letters a-z are allowed
+            if (charCode > 97 && charCode < 122) {
+                // Ascii code for a starts at 97, so minus 1 to get the alphabet position
+                charCode = charCode - 96;
+                output = output + String.valueOf(charCode) + " ";
+            }
+        }
+        System.out.println(output);
+    }
+}
